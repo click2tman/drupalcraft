e8f01bce826600c222b9a021bea4db8292359838
Patrick Coffey <patrickcoffey48@gmail.com>
2015-01-19 13:53:52 -0600

    Add behat test task file

---
+++ _tasks/behat.task.js
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * The following code defines testing tasks with Behat.
+ */
+/* globals require */
+var gulp = require('gulp'),
+    merge = require('merge-stream'),
+    gutil = require('gulp-util'),
+    options = require('minimist')(process.argv.slice(2));
+
+
+
